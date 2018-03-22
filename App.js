f887085440bd7e4dccbcd2bad06cbd0d134324a1
Flavio Iozzoli <fiozzoli@usersad.everis.int>
2018-03-22 17:57:04 -0300

    Inicial

--- App.js
+++ App.js
@@ -2,13 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import Header from './components/layouts/Header';
+import './styles/main.css'
 
 class App extends React.Component {
   render() {
     return (
-      <MuiThemeProvider>
-        <Header />
-      </MuiThemeProvider>
+      <div>
+        <MuiThemeProvider>
+          <Header />
+        </MuiThemeProvider>
+        <div className="content">
+          asdsadsadsa
+        </div>
+      </div>
     );
   }
 }
--- components/layouts/Header.js
+++ components/layouts/Header.js
@@ -2,13 +2,16 @@ import React from 'react';
 import AppBar from 'material-ui/AppBar';
 import Drawer from 'material-ui/Drawer';
 import MenuItem from 'material-ui/MenuItem';
+import '../../styles/header.css'
  
 class Header extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      open: true,
-      drawer_docked: false,
+      open: window.innerWidth > 991,
+      manual_toggle : false,
+      appbar_class : window.innerWidth > 991 ? 'sidebar_opened' : 'sidebar_closed',
+      drawer_docked: window.innerWidth > 991,
       width: window.innerWidth
     };
   }
@@ -21,32 +24,39 @@ class Header extends React.Component {
     window.removeEventListener('resize', this.handleWindowSizeChange);
   }
 
-  handleToggle = () => this.setState({open: !this.state.open});
+  handleToggle = () => {
+    this.setState({open: !this.state.open});
+    this.setState({manual_toggle: true});
+    if(!this.state.open){
+      this.setState({appbar_class: 'sidebar_opened'});
+    }else{
+      this.setState({appbar_class: 'sidebar_closed'});
+    }
+  }
 
-  handleClose = () => this.setState({open: false});
+  handleSidebar = (isNotMobile) => {
+    if(!this.state.manual_toggle){
+      this.setState({open: isNotMobile});
+    }
+    this.setState({drawer_docked: isNotMobile});
+  }
 
   handleWindowSizeChange = () => {
     this.setState({ width: window.innerWidth });
-
-    if(this.state.width < 991){
-      this.setState({open: false});
-      this.setState({drawer_docked: false});
-    }else{
-      this.setState({open: true});
-      this.setState({drawer_docked: true});
-    }
+    this.handleSidebar(this.state.width > 991);
   };
 
   render(){
     return (
       <div>
         <AppBar 
+          className = {this.state.appbar_class}
           onLeftIconButtonClick = {this.handleToggle} 
         />
         <Drawer 
           open={this.state.open}
           docked={this.state.drawer_docked}
-          onRequestChange={(open) => this.setState({open})}
+          onRequestChange={this.handleToggle}
         >
           <MenuItem>Menu Item</MenuItem>
           <MenuItem>Menu Item 2</MenuItem>
